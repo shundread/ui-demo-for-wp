fa77ecd3206881949c37b69d05a9879097fe4aac
Thiago Chaves de Oliveira Horta <thiago.coh@gmail.com>
2025-03-26 19:51:19 +0200

    More visual improvements

--- src/components/GatewayDetail/GatewayDetailTimelineRow.tsx
+++ src/components/GatewayDetail/GatewayDetailTimelineRow.tsx
@@ -18,23 +18,38 @@ export function GatewayDetailTimelineRow({
     timeInStatusesS,
     statusChangeEvents,
 }: GatewayDetailTimelineRowProps) {
-    const durationS = (endTime.valueOf() - startTime.valueOf()) / 1000;
+    // For most bins the following will do:
+    //
+    //      const durationS = (endTime.valueOf() - startTime.valueOf()) / 1000;
+    //
+    // However at the extremities this doesn't hold true, so we do this workaround:
+    const durationS = timeInStatusesS.active + timeInStatusesS.inactive + timeInStatusesS.unstable + timeInStatusesS.offline;
     if (durationS <= 0) throw new Error(`Invalid gateway stats history sample duration: ${durationS}`);
 
-    const opacityActive = timeInStatusesS.active / durationS;
-    const opacityInactive = timeInStatusesS.inactive / durationS;
-    const opacityUnstable = timeInStatusesS.unstable / durationS;
-    const opacityOffline = timeInStatusesS.offline / durationS;
+    const shareActive = timeInStatusesS.active / durationS;
+    const shareInactive = timeInStatusesS.inactive / durationS;
+    const shareUnstable = timeInStatusesS.unstable / durationS;
+    const shareOffline = timeInStatusesS.offline / durationS;
+
+    const numberFormatter = Intl.NumberFormat("en-US", { style: "percent", maximumFractionDigits: 2 });
 
     return (
         <tr>
             <td className="gateway-stats-timeline-table-body-timestamp">
                 {index % 5 === 0 ? <span>{shortDateStr(startTime)} - {timeStr(startTime)}</span> : null}
             </td>
-            <td className="gateway-stats-timeline-table-body-active" style={{ opacity: opacityActive }}></td>
-            <td className="gateway-stats-timeline-table-body-inactive" style={{ opacity: opacityInactive }}></td>
-            <td className="gateway-stats-timeline-table-body-unstable" style={{ opacity: opacityUnstable }}></td>
-            <td className="gateway-stats-timeline-table-body-offline" style={{ opacity: opacityOffline }}></td>
+            <td className="gateway-stats-timeline-table-body-active" style={{ opacity: shareActive ? 1 : 0 }}>
+                {shareActive > 0 ? <span>{numberFormatter.format(shareActive)}</span> : null}
+            </td>
+            <td className="gateway-stats-timeline-table-body-inactive" style={{ opacity: shareInactive ? 1 : 0 }}>
+                {shareInactive > 0 ? <span>{numberFormatter.format(shareInactive)}</span> : null}
+            </td>
+            <td className="gateway-stats-timeline-table-body-unstable" style={{ opacity: shareUnstable ? 1 : 0 }}>
+                {shareUnstable > 0 ? <span>{numberFormatter.format(shareUnstable)}</span> : null}
+            </td>
+            <td className="gateway-stats-timeline-table-body-offline" style={{ opacity: shareOffline ? 1 : 0 }}>
+                {shareOffline > 0 ? <span>{numberFormatter.format(shareOffline)}</span> : null}
+            </td>
             <td className="gateway-stats-timeline-table-body-notes">
                 <GatewayDetailTimelineRowEvents startTime={startTime} endTime={endTime} statusChangeEvents={statusChangeEvents} />
             </td>
